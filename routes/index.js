f2401513e18d6a64fcafbbbca202ad0fe9f0be62
Matteo AUGER <augermtt@gmail.com>
2019-11-19 13:55:06 +0100

    Update index.js to fit the new app architecture

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.statusCode = 200;
+  res.send();
 });
 
 module.exports = router;
