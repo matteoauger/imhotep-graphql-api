06175c7ea5b930af9cab04483a723ebdadf0497e
Matteo AUGER <matteo.auger@etu.univ-lehavre.fr>
2019-11-26 22:54:44 +0100

    Update ad-service.js add price based query methods

--- services/ad-service.js
+++ services/ad-service.js
@@ -8,6 +8,22 @@ class AdService {
     getAd(data) {
         return Ad.find(data);
     }
+
+    getAdsCheaperThan(price, inclusive) {
+        let query = { price: { $lt : price }};
+        if (inclusive)
+            query = { price : { $lte: price }};
+
+        return Ad.find(query);
+    }
+
+    getAdsMoreExpensiveThan(price, inclusive) {
+        let query = { price: { $gt : price }};
+        if (inclusive)
+            query = { price : { $gte: price }};
+
+        return Ad.find(query);
+    }
 }
 
 module.exports = AdService;
