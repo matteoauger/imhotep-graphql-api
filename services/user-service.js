23f4d33232f00f737e23505f71bb29a6de77464f
Yoann Eichelberger <eichelberger.y@gmail.com>
2019-12-03 03:09:24 +0100

    Add user service

---
+++ services/user-service.js
@@ -0,0 +1,26 @@
+const UserSchema = require('../models/user');
+const User = UserSchema.User;
+
+/**
+ * Service fetching and modifying data from the user database.
+ */
+class UserService {
+
+    getUser(data) {
+        return User.find(data);
+    }
+
+    insert(data) {
+        return User.create(data);
+    }
+
+    update(id, data) {
+        return User.updateOne({ _id: id }, data);
+    }
+
+    delete(id) {
+        return User.deleteOne({ _id: id });
+    }
+}
+
+module.exports = UserService;
