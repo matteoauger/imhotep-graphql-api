de2cdeb0fb9c6f1ea65c6a6812eca92eaea905da
Yoann Eichelberger <eichelberger.y@gmail.com>
2019-12-03 03:10:11 +0100

    Add user query handler

---
+++ graphql/user.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const { buildSchema } = require('graphql');
+const UserService = require('../services/user-service');
+const UserDataService = require('../services/user-data-service');
+const path = require('path');
+
+class UserQueryHandler {
+    constructor() {
+        this.userService = new UserService();
+        this.userDataService = new UserDataService();
+    }
+
+    async user(data) {
+        const params = this.userDataService.prepareDataForDb(data);
+        const ads = await this.userService.getAd(params);
+
+        return this.userDataService.prepareDataForGql(ads);
+    }
+
+    async register(data) {
+        const params = this.adDataService.prepareDataForDb(data);
+        const newUser = await this.userService.insert(params);
+        const result = this.adDataService.prepareDataForGql([ newUser ]);
+
+        return result[0];
+    }
+}
+
+// reading graphQL ad schema from gql file
+const schemaPath = path.join(__dirname, 'gql-schemas', 'User.gql');
+const schemaTxt = fs.readFileSync(schemaPath,  { encoding: 'utf-8' });
+
+module.exports = {
+    schema: buildSchema(schemaTxt),
+    root: new UserQueryHandler()
+}
